422d7de279a465c89003d8a04e654839062b9dca
PeterMazzoli <Peter.mazzoli@gmail.com>
2017-03-09 16:24:15 -0500

    heroku

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+var friends = [
+  {
+  	"name":"Harry Potter",
+  	"photo":"https://s-media-cache-ak0.pinimg.com/originals/e2/50/a2/e250a240c9a6ef39edc4fe4be85f47ca.jpg",
+  	"scores":[
+	    "5",
+	    "1",
+	    "4",
+        "4",
+	    "5",
+	    "1",
+	    "2",
+	    "5",
+	    "4",
+	    "1"
+    ]
+  },
+  {
+  	"name": "Ron Weasley",
+	"photo": "https://upload.wikimedia.org/wikipedia/en/5/5e/Ron_Weasley_poster.jpg",
+	"scores": [
+	 	"4",
+		"2",
+		"5",
+		"1",
+		"3",
+		"2",
+		"2",
+		"1",
+		"3",
+		"2"
+	]
+},
+{
+	"name": "Hermione Granger",
+	"photo": "https://upload.wikimedia.org/wikipedia/en/d/d3/Hermione_Granger_poster.jpg",
+	"scores": [
+		"5",
+		"2",
+		"2",
+		"2",
+		"4",
+		"1",
+		"3",
+		"2",
+		"5",
+		"5"
+	]
+},
+{
+	"name": "Lord Voldemort",
+	"photo": "https://img.buzzfeed.com/buzzfeed-static/static/2016-03/6/15/campaign_images/webdr08/how-lord-voldemort-are-you-g2wy-2-6896-1457296977-1_dblbig.jpg",
+	"scores": [
+		"3",
+		"3",
+		"4",
+		"2",
+		"2",
+		"1",
+		"3",
+		"2",
+		"2",
+		"3"
+	]
+},
+{
+	"name": "Albus Dumbledore",
+	"photo": "https://ladygeekgirl.files.wordpress.com/2014/01/albus-dumbledore.jpg",
+	"scores": [
+		"4",
+		"3",
+		"4",
+		"1",
+		"5",
+		"2",
+		"5",
+		"3",
+		"1",
+		"4"
+	]
+},
+{
+	"name": "Severus Snape",
+	"photo": "http://vignette2.wikia.nocookie.net/harrypotter/images/e/e1/Severus_Snape_Profile.JPG/revision/latest/scale-to-width-down/145?cb=20081103135333",
+	"scores": [
+		"4",
+		"4",
+		"2",
+		"3",
+		"2",
+		"2",
+		"3",
+		"2",
+		"4",
+		"5"
+	]
+  }
+	
+]
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+// BodyParser makes it possible for the server to interpret data sent to it.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use(bodyParser.json({ type: 'application/*+json' }))
+
+// app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+
+// app.use(bodyParser.text({ type: 'text/html' }))
+
+// require("./app/routing/apiRoutes.js")(app);
+// require("./app/routing/htmlRoutes.js")(app);
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
+
+// app.listen(PORT, function() {
+// 	console.log("App listening on PORT: " + PORT);
+// });
